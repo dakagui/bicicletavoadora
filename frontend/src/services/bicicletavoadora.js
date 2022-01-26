f2bffecf13a671bb8c59a5f9605df27c8e50b9c9
dakagui <daniakagui@gmail.com>
2022-01-26 07:30:37 -0500

    adiciona estrutura de testes

--- frontend/src/services/bicicletavoadora.js
+++ frontend/src/services/bicicletavoadora.js
@@ -3,7 +3,6 @@ import API from "./Api"
 export const getHello = async () => {
     try {
         const response = await API.get("/hello");
-        console.log(response);
         return response;
     } catch (error){
         console.error(error);
